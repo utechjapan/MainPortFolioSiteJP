6d08b5703227fff7dc79620963262d5eeeb19b74
UTechJapan || UTechLab <chikara.inohara@utechjapan.net>
2025-03-10 05:30:31 +0000

    few adjustment

--- lib/siteConfig.ts
+++ lib/siteConfig.ts
@@ -66,7 +66,7 @@ export const siteConfig = {
   socialLinks: [
     {
       name: "LinkedIn",
-      url: "https//www.linkedin.com/in/chikara-inohara",
+      url: "https://ph.linkedin.com/in/chikara-inohara-a26bb9143",
       icon: "fab fa-linkedin",
     },
     {
